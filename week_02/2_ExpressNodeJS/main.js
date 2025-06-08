81ef9c51b8cadf9dfe616154c17657283fae8cd1
MukeshMB012 <nirvanamb012@gmail.com>
2025-06-08 22:31:14 +0530

    express nodejs added

---
+++ week_02/2_ExpressNodeJS/main.js
@@ -0,0 +1,62 @@
+// http server 1 
+// how to get package.json file(npm init -y:- run in terminal)
+// how to get express(package-lock.json file + node_modules) lockelly(npm install express:- run in terminal)
+// for taking the input from the user, need to install body parser(npm install body-parser)
+// instead of using (node main.js) after every changes in the code, must use (nodemon main.js) it will automatically updates the changes in the terminal
+ 
+// const fs = require("fs");  similar to this the below one
+// const express = require("express");
+
+
+// // Actual code of the pdf
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
+
+ 
+// rough code
+const express = require('express');
+const bodyParser = require("body-parser"); // adding new library body-parser for taking the input from the user
+
+const app = express();
+const port = 3000;  //process.env.PORT?
+// const port = process.env.POST || 3000 //:- find out about this
+
+// middleware(below line code)
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.send('Hello World!')
+})
+
+app.get("/me", function(req, res){
+    res.json({
+        name: "mukesh",
+        age: 20
+    })
+})
+
+// Authorization(how to get)  
+app.post('/conversation', function(req, res){
+    console.log(req.headers)  //this will give you the whole bio data of headers
+    console.log(req.headers["authorization"])  //this will only give you the authorization number(which you passes here for this case)
+    // console.log(req.body)  //:-(taking input from the users)  it won’t work as expected unless you add middleware to parse the request body.
+    
+    // const message =req.query.message;  //(it allows the input from the route:- ?(now write your input))
+    // console.log(message);
+    const message =req.body.message;
+    console.log(message);
+    res.send({
+        msg: "2+2=4"
+    })
+})
+
+app.listen(port);
---
+++ week_02/2_ExpressNodeJS/notes.md
@@ -0,0 +1,6 @@
+chatGPT notes(whole pdf) links:-
+https://chatgpt.com/share/68451d9c-2020-8011-a785-5880c0fee9f5
+
+
+chatGPT links to understand each line of code for creating http:-
+https://chatgpt.com/share/68454090-3b6c-8011-aa64-3c5407feb6b4
