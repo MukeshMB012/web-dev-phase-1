f46a3e504d27e4e6e277ec2b29743ab15fb8eb7c
MukeshMB012 <nirvanamb012@gmail.com>
2025-09-27 04:17:21 +0530

    revision week 01

--- try.js
+++ try.js
@@ -7,3 +7,26 @@
 //     res.json({});
 
 // })
+
+// static method
+class Animal {
+  constructor(name, legCount, speaks) {
+    this.name = name;
+    this.legCount = legCount;
+    this.speaks = speaks;
+  }
+//   static method
+  static myType(){
+    console.log("Animal ha ha" + this.speaks);
+  }
+//   Instance Method
+  speak(){
+    console.log("hi there" + this.speaks);
+  }
+}
+
+Animal.myType()  // static function is called on class not object
+let dog = new Animal("dog", 4, " bhow bhow"); //create object
+let cat = new Animal("cat", 4, " meow meow");
+dog.speak(); //call instance function on object
+cat.speak();
